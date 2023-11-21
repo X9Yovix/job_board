6c6c4acf8720ae64d33ddcb17af5402f71501873
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-21 19:12:10 +0100

    adding necessary columns for registration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use DateInterval;
+use DateTime;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -28,6 +31,24 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $registrationToken = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $registrationTokenLifeTime = null;
+
+    #[ORM\Column]
+    private ?bool $verified = null;
+
+    private $tokenLifetime;
+
+    public function __construct()
+    {
+        $this->verified = false;
+        $this->tokenLifetime = getenv('REGISTRATION_TOKEN_LIFETIME');
+        $this->registrationTokenLifeTime = (new DateTime('now'))->add(new DateInterval($this->tokenLifetime));
+    }
 
     public function getId(): ?int
     {
@@ -97,5 +118,41 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getRegistrationToken(): ?string
+    {
+        return $this->registrationToken;
+    }
+
+    public function setRegistrationToken(?string $registrationToken): static
+    {
+        $this->registrationToken = $registrationToken;
+
+        return $this;
+    }
+
+    public function getRegistrationTokenLifeTime(): ?\DateTimeInterface
+    {
+        return $this->registrationTokenLifeTime;
+    }
+
+    public function setRegistrationTokenLifeTime(\DateTimeInterface $registrationTokenLifeTime): static
+    {
+        $this->registrationTokenLifeTime = $registrationTokenLifeTime;
+
+        return $this;
+    }
+
+    public function isVerified(): ?bool
+    {
+        return $this->verified;
+    }
+
+    public function setVerified(bool $verified): static
+    {
+        $this->verified = $verified;
+
+        return $this;
     }
 }
