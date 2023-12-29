70c65ba6f1a6b5e54751924c8dce1598e88d931f
nada ghribi <nada.ghribi203@gmail.com>
2023-12-29 12:28:14 +0100

    change password

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -3,14 +3,18 @@
 namespace App\Controller\User;
 
 use App\Entity\Country;
+use App\Form\ChangePasswordType;
 use App\Form\UserType;
 use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+
 
 #[IsGranted("ROLE_USER")]
 #[Route('/user')]
@@ -80,4 +84,43 @@ class UserController extends AbstractController
             'user' => $user,
         ]);
     }
+
+#[Route('/updatePassword', name: 'update_password')]
+public function updatePassword(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+
+{
+
+    /**
+ * @var \App\Entity\User $user
+ */
+    $user = $this->getUser();
+    $formData = [
+        'currentPassword' => $user->getPassword(),
+        'newPassword' => '',
+        'confirmPassword' => '',
+    ];
+    $form = $this->createForm(ChangePasswordType::class,$formData);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $formData = $form->getData();
+        if (!$passwordHasher->isPasswordValid($user, $formData['currentPassword'])) {
+            $this->addFlash('error', 'The current password is incorrect.');
+        } elseif ($formData['newPassword'] !== $formData['confirmPassword']) {
+            $this->addFlash('error', 'The new password and confirmation do not match.');
+        } else {
+
+            $encodedPassword = $passwordHasher->encodePassword($user, $formData['newPassword']);
+            $user->setPassword($encodedPassword);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Password changed successfully.');
+            return $this->redirectToRoute('app_user_profile');
+        }
+    }
+    return $this->render('user/updatePassword.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
+
 }
---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('currentPassword', PasswordType::class, [
+                'label' => 'Current Password',
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'New Password'],
+                'second_options' => ['label' => 'Confirm Password'],
+            ])
+            ;
+    }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
